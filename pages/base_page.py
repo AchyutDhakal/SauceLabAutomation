4f3d77a43d4df1b38ac36d64bd9722dc3cf8e2b9
Achyut Dhakal <achyutdhakal006@gmail.com>
2026-01-13 16:40:58 +0545

    Failure screenshots attachment added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import allure
+import os
+import time
 
 class BasePage():
     def __init__(self,driver):
@@ -17,13 +20,25 @@ class BasePage():
         return self.wait.until(EC.visibility_of_element_located(locator))
     
     def click_element(self,locator):
-        element = self.find_clickable_element(locator)
-        element.click()
+        with allure.step(f"Clicking element with xpath {locator}"):
+            try:
+                element = self.find_clickable_element(locator)
+                element.click()
+                self.take_screenshot()
+
+            except Exception as e:
+                self.take_screenshot()
 
     def enter_text(self,locator, text):
-        element = self.find_element(locator)
-        element.clear()
-        element.send_keys(text)
+        with allure.step(f"Enter text using send keys for element with xpath {locator}"):
+            try:
+                element = self.find_element(locator)
+                element.clear()
+                element.send_keys(text)
+
+            except Exception as e:
+                self.take_screenshot()
+                raise e
 
     def is_element_present(self, locator):
         try:
@@ -40,6 +55,25 @@ class BasePage():
         
         except TimeoutException:
             return False
+
+    @allure.step("Take Screenshot")
+    def take_screenshot(self):
+        name = "Screenshot"
+        screenshots_dir = "Screenshots"
+
+        if not os.path.exists(screenshots_dir):
+            os.makedirs(screenshots_dir)
+
+        timestamp_str = int(time.time())
+        file_name = f"{screenshots_dir}/{name}_{timestamp_str}.png"
+
+        self.driver.save_screenshot(file_name)
+
+        allure.attach.file(
+            file_name,
+            name=name,
+            attachment_type = allure.attachment_type.PNG
+        )
         
 
 
--- tests/test_login.py
+++ tests/test_login.py
@@ -23,40 +23,31 @@ class TestSignup:
     @allure.title("Signup Testing")
     @pytest.mark.parametrize("valid_data", VALID_DATA )
     def test_signup(self, valid_data):
-        with allure.step("Click signup link"):
-            self.login_page.click_signup_link()
-        
-        with allure.step("Signup link click assertion"):
-            assert_true("register" in self.driver.current_url, "User is not on the signup page")
+        self.login_page.click_signup_link()     
+        assert_true("register" in self.driver.current_url, "User is not on the signup page")
 
-        with allure.step("Fill out the signup form"):
-            self.login_page.enter_firstname_signup(valid_data["first_name"])
-            self.login_page.enter_lastname_signup(valid_data["last_name"])
-            self.login_page.enter_email_signup(valid_data["email"])
-            self.login_page.enter_password_signup(valid_data["signup_password"])
+        self.login_page.enter_firstname_signup(valid_data["first_name"])
+        self.login_page.enter_lastname_signup(valid_data["last_name"])
+        self.login_page.enter_email_signup(valid_data["email"])
+        self.login_page.enter_password_signup(valid_data["signup_password"])
 
-        with allure.step("Click signup button"):
-            self.login_page.click_signup_button()
+        self.login_page.click_signup_button()
 
-        with allure.step("Signup Assertion"):
-            time.sleep(3)
-            assert_true(self.login_page.is_element_visible(self.login_page.LOG_OUT), "Signup failed as logout is not present on the landing page.")
+        time.sleep(3)
+        assert_true(self.login_page.is_element_visible(self.login_page.LOG_OUT), "Signup failed as logout is not present on the landing page.")
 
     @allure.title("Login Test with valid test data")
     @pytest.mark.parametrize("valid_data",VALID_DATA)
     def test_valid_login(self, valid_data):
-        with allure.step("Click login link"):
-            self.login_page.click_login_link()
+        self.login_page.click_login_link()
 
-        with allure.step("Login link click assertion"):
-             assert_true("login" in self.driver.current_url, "User is not on the login page")
+
+        assert_true("login" in self.driver.current_url, "User is not on the login page")
         
-        with allure.step("Fill out login form and click login button"):
-            self.login_page.enter_username(valid_data['username'])
-            self.login_page.enter_password(valid_data['password'])
-            self.login_page.click_login()
+        self.login_page.enter_username(valid_data['username'])
+        self.login_page.enter_password(valid_data['password'])
+        self.login_page.click_login()
 
-        with allure.step("Login Assertion"):
-            time.sleep(3)
-            assert_true(self.login_page.is_element_visible(self.login_page.LOG_OUT), "Login failed as logout button is not present")
+        time.sleep(3)
+        assert_true(self.login_page.is_element_visible(self.login_page.LOG_OUT), "Login failed as logout button is not present")
 
